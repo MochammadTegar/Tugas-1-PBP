66ab42b8be117fac09c118c2712ac680b88e8501
MochammadTegar <153746704+MochammadTegar@users.noreply.github.com>
2024-09-24 09:07:32 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,11 @@
+const nama = document.querySelector('#Name');
+nama.textContent = 'M Tegar EB';
+
+const ttl = document.querySelector('#ttl');
+ttl.textContent = 'Sukabumi, 21 Agustus 2003';
+
+const alamat = document.querySelector('#alamat');
+alamat.textContent = 'KP Nagrak Kaler RT 17/03 Kec Cisaat Kab Sukabumi';
+
+const agama = document.querySelector('#agama');
+agama.textContent = 'Islam';
